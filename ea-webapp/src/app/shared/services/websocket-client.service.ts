49951b9ea31dc2ccdd78f1f150232def8a0c5613
Thomas Brian <tbrian@appriver.com>
2017-11-21 11:14:20 -0600

    Setup ea-shared dependency

--- ea-webapp/src/app/shared/services/websocket-client.service.ts
+++ ea-webapp/src/app/shared/services/websocket-client.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
-import {  } from 'ea-shared/models';
+import { ServerMessage } from 'ea-shared/models';
 
 @Injectable()
 export class WebsocketClientService {
